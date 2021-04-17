177983e59425a43e749979b89ad882cadc0c278e
pivasikpivtsakov <50816300+pivasikpivtsakov@users.noreply.github.com>
2021-04-18 03:51:16 +0500

    contact getter

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ProducesResponseType(401)]
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ContactController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// возвращает все контакты текущего пользователя
+        /// </summary>
+        [HttpGet("ThisUserContacts")]
+        public async Task<IActionResult> GetThisUserContacts()
+        {
+            var userId = User.Identity?.Name;
+            var contacts = await _applicationDbContext.UsersToContacts
+                .Where(x => x.UserId == userId)
+                .Select(x => x.Contact)
+                .ToListAsync();
+            return Ok(contacts);
+        }
+        
+        /// <summary>
+        /// добавляет указанного пользователя этому пользователю в контакты (не завершено)
+        /// </summary>
+        [HttpPost("ThisUserContacts")]
+        public async Task<IActionResult> AddContactToThisUser(string chatId)
+        {
+            var userId = User.Identity?.Name;
+            await _applicationDbContext.UsersToContacts
+                .AddAsync(new UserToContact
+                {
+                    
+                });
+            return Ok();
+        }
+    }
+}
