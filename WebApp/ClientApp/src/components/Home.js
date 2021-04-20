d700d17403f89716c8eaa5affbe478bda6e6102e
goltisar <goltisar@gmail.com>
2021-04-20 17:39:41 +0500

    Updated NavMenu, added "beautiful" scrollbar

--- WebApp/ClientApp/src/components/Home.js
+++ WebApp/ClientApp/src/components/Home.js
@@ -5,21 +5,28 @@ export class Home extends Component {
 	static displayName = Home.name;
 	state = {
 		text: '',
-		firstLine: '',
-		firstLineLength: 0
 	}
+
 	textChange(text){
-		// if (this.state.firstLineLength > 125){
-		// 	console.log("больше");
-		// 	this.setState({
-		// 		text: this.state.text + this.state.firstLine,
-		// 		firstLine: text,
-		// 		firstLineLength: this.state.firstLine.length
-		// 	})
-		// } else {
 		this.setState({
 			text: this.state.text + text,
 		});
+		if(text !== "")
+			setTimeout(() => {
+				if (document.querySelector('.chat-text').scrollHeight > document.querySelector('.chat-place').clientHeight - 10)
+					this.scrollDown();
+			}, 10)
+	}
+	scrollDown(){
+		let chatBody = document.querySelector('.chat-text');
+		chatBody.scrollTop = chatBody.scrollHeight;
+	}
+	scrollHandle(){
+		document.querySelector(".chat-input").blur()
+		let scrollHeight = document.querySelector('.chat-text').scrollHeight;
+		let scrollTop = document.querySelector('.chat-text').scrollTop
+		if (scrollHeight - scrollTop === 800) // 800 высота чатека, надо будет взять из свойства
+			setTimeout(function () { document.querySelector(".chat-input").focus()}, 11);
 	}
 	// phraseComponent -
 	// userContact - component сделать
@@ -32,22 +39,20 @@ export class Home extends Component {
 						<li>Макар</li>
 						<li>Димас</li>
 						<li>Личка с собой???</li>
-						<li>Иван Домашних</li>
+						<li>Надо нажать на область, чтоб писать -&gt;</li>
 					</ul>
 				</div>
 				<div className="chat-place" onClick={() => {
 					document.querySelector(".chat-input").focus();
+					this.scrollDown()
 				}}>
-
 					<div className="chat-text-wrap">
-						<div className="chat-container">
-							<div className="chat-text">{this.state.text}
-								<input type="text"
-								       className="chat-input"
-								       value=""
-								       onChange={(event => {this.textChange(event.target.value)})}
-								/></div>
-
+						<div className="chat-text" onScroll={() => {this.scrollHandle()}}>{this.state.text}
+							<input type="text"
+							       className="chat-input"
+							       value=""
+							       onChange={(event => {this.textChange(event.target.value)})}
+							/>
 						</div>
 					</div>
 				</div>
