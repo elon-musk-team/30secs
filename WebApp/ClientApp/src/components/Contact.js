87707fbd4b7024e8b2da5089abc030d79c7793d6
pivasikpivtsakov <50816300+pivasikpivtsakov@users.noreply.github.com>
2021-04-22 13:46:16 +0500

    more beautiful list

--- WebApp/ClientApp/src/components/Contact.js
+++ WebApp/ClientApp/src/components/Contact.js
@@ -5,15 +5,18 @@ export class Contact extends Component{
     
     render() {
         return (
-            <li className="container contact-container row">
-                <div className="col">
-                    <div className="avatar-circle">
-                        <img src={this.props.avatarLink} alt="-_-"/>
+            <li className="list-group-item flex-row align-items-start">
+                <div className="row">
+                    <div className="col">
+                        <div className="avatar-circle">
+                            <img src={this.props.avatarLink} alt="-_-"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p className="text-truncate contact-label flex-grow-1">{this.props.screenName}</p>
                     </div>
                 </div>
-                <div className="col">
-                    <p className="text-truncate contact-label">{this.props.screenName}</p>
-                </div>
+                
             </li>
         )
     }
--- WebApp/ClientApp/src/components/Home.js
+++ WebApp/ClientApp/src/components/Home.js
@@ -61,7 +61,7 @@ export class Home extends Component {
 		return (
 			<div className="main-page">
 				<div className="user-contacts">
-					<ul className="contacts">
+					<ul className="list-group list-group-flush contacts">
 						<Contact avatarLink="https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg" screenName="sample_comtact_umgmg"/>
 						{this.state.contactDtos.map(value => 
 							<Contact avatarLink={value.avatarLink} screenName={value.screenName}/>
