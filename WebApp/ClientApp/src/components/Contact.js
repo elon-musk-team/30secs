cf17e3a2f6f5a4f6b134d89ed5e26022eeb3642f
pivasikpivtsakov <50816300+pivasikpivtsakov@users.noreply.github.com>
2021-04-28 16:56:36 +0500

    better css

--- WebApp/ClientApp/src/components/Contact.js
+++ WebApp/ClientApp/src/components/Contact.js
@@ -1,23 +1,18 @@
 ﻿import {Component} from "react";
 
-export class Contact extends Component{
+export class Contact extends Component {
     static displayName = Contact.name;
-    
+
     render() {
         return (
-            <li className="list-group-item flex-row align-items-start">
-                <div className="row">
-                    <div className="col">
-                        <div className="avatar-circle">
-                            <img src={this.props.avatarLink} alt="-_-"/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <p className="text-truncate contact-label flex-grow-1">{this.props.screenName}</p>
-                    </div>
+            <div className="user">
+                <div className="avatar">
+                    <img className="avatar-circle" src={this.props.avatarLink} alt="User name"/>
+                    <div className="status off"/>
                 </div>
-                
-            </li>
+                <div className="ct-name">{this.props.screenName}</div>
+                <div className="ct-mood">additional info might go here</div>
+            </div>
         )
     }
 }
