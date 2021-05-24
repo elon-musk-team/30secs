1d527cb6585b7658cab8168e0e8c8a16d18b408d
pivasikpivtsakov <50816300+pivasikpivtsakov@users.noreply.github.com>
2021-05-25 02:17:49 +0500

    selection of contact is visible

--- WebApp/ClientApp/src/components/Contact.js
+++ WebApp/ClientApp/src/components/Contact.js
@@ -3,9 +3,19 @@
 export class Contact extends Component {
     static displayName = Contact.name;
 
+    constructor(props) {
+        super(props);
+    }
+    
+    isSelectedToClass(){
+        return this.props.isSelected 
+            ? " selected" 
+            : "";
+    }
+    
     render() {
         return (
-            <div className="user" onClick={this.props.onClick}>
+            <div className={"user" + this.isSelectedToClass()} onClick={this.props.onClick}>
                 <div className="avatar">
                     <img className="avatar-circle" src={this.props.avatarLink} alt="User name"/>
                     <div className="status off"/>
--- WebApp/ClientApp/src/components/Home.js
+++ WebApp/ClientApp/src/components/Home.js
@@ -125,9 +125,10 @@ export class Home extends Component {
                         <Contact avatarLink="https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg"
                                  screenName="sample_comtact_umgmg" onClick={async () => await this.selectPeer("sample_comtact_umgmg")}/>
                         {this.state.contactDtos.map(value =>
-                            <Contact avatarLink={value.avatarLink}
+                            <Contact avatarLink={value.avatarLink} 
                                      screenName={value.screenName}
                                      key={value.screenName}
+                                     isSelected={this.state.selectedPeerName === value.screenName}
                                      onClick={async () => await this.selectPeer(value.screenName)}/>
                         )}
                     </ul>
